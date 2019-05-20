17669c7bda836c85e51911754ef6d936eda9bb38
roluisker <roluisker@gmail.com>
2019-05-19 19:56:56 -0500

    [chart] -> line, pending

--- timeline/src/main/java/com/sc/timeline/ui/HistoricalFragment.kt
+++ timeline/src/main/java/com/sc/timeline/ui/HistoricalFragment.kt
@@ -1,6 +1,7 @@
 package com.sc.timeline.ui
 
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import androidx.lifecycle.MutableLiveData
 import com.sc.core.BaseFragment
@@ -12,17 +13,63 @@ import com.sc.timeline.HistoricalViewModel
 import com.sc.timeline.R
 import com.sc.timeline.di.DaggerTimelineComponent
 import com.sc.timeline.ui.di.HistoricalFragmentModule
+import kotlinx.android.synthetic.main.historical_fragment.*
 import timber.log.Timber
 import javax.inject.Inject
+import lecho.lib.hellocharts.model.*
+import lecho.lib.hellocharts.model.Viewport
 
 class HistoricalFragment : BaseFragment() {
 
     @Inject
     lateinit var historicalViewModel: HistoricalViewModel
 
+    // https://www.codingdemos.com/draw-android-line-chart/
+
+    var axisData = arrayOf("Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec")
+    var yAxisData = intArrayOf(50, 20, 15, 30, 20, 60, 15, 40, 45, 10, 90, 18)
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         historicalViewModel.showHistorical("2012-05-01", "2012-05-25")
+
+        val yAxisValues = ArrayList<PointValue>()
+        val axisValues = ArrayList<AxisValue>()
+
+        val line = Line(yAxisValues).setColor(Color.parseColor("#9C27B0"))
+
+        for (i in 0 until axisData.size) {
+            axisValues.add(i, AxisValue(i.toFloat()).setLabel(axisData[i]))
+        }
+
+        for (i in 0 until yAxisData.size) {
+            yAxisValues.add(PointValue(i.toFloat(), yAxisData[i].toFloat()))
+        }
+
+        val lines = ArrayList<Line>()
+        lines.add(line)
+
+        val data = LineChartData()
+        data.lines = lines
+
+        val axis = Axis()
+        axis.setValues(axisValues)
+        axis.setTextSize(16)
+        axis.setTextColor(Color.parseColor("#03A9F4"))
+        data.axisXBottom = axis
+
+        val yAxis = Axis()
+        yAxis.name = "Sales in millions"
+        yAxis.textColor = Color.parseColor("#03A9F4")
+        yAxis.textSize = 16
+        data.axisYLeft = yAxis
+
+        lineChart.setLineChartData(data)
+        val viewport = Viewport(lineChart.getMaximumViewport())
+        viewport.top = 110f
+        lineChart.setMaximumViewport(viewport)
+        lineChart.setCurrentViewport(viewport)
+
     }
 
     override fun onAttach(context: Context?) {
