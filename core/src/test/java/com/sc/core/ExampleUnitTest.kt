cbb2fefdc70387e29c989dd68f0aba47f9336d32
roluisker <roluisker@gmail.com>
2019-05-26 12:43:48 -0500

    [gradle]

---
+++ core/src/test/java/com/sc/core/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.sc.core
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+import org.junit.Assert.*
+
+@RunWith(JUnit4::class)
+class ExampleUnitTest {
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, (2 + 2).toLong())
+    }
+
+}
